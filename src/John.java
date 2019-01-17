402c48ee1776c9003953ab558ddacd7d30d5fe27
CyberTekFA <faiz.m.amiree@gmail.com>
2019-01-16 22:07:35 -0500

    someThing

--- src/John.java
+++ src/John.java
@@ -1,8 +1,10 @@
 public class John {
     public static void main(String[] args) {
 
-        for (int i = 0; i <10 ; i++) {
-            System.out.println("%d,%d,%d "+5*i);
+        for (int i = 0; i < 10; i++) {
+
+
+            System.out.println(5 * i);
         }
     }
 }
